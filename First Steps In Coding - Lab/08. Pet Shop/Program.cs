cf2ed5a7a8bc624973c2735b20ef04017faab7ff
SelcukBeysim <118006658+SelcukBeysim@users.noreply.github.com>
2024-01-08 19:16:11 +0200

    First Steps In Coding - Lab

---
+++ First Steps In Coding - Lab/08. Pet Shop/Program.cs
@@ -0,0 +1,6 @@
+﻿double dog = int.Parse(Console.ReadLine());
+int cat = int.Parse(Console.ReadLine());
+int cats = cat * 4;
+double dogs = dog * 2.50;
+double final = dogs + cats;
+Console.WriteLine($"{final} lv.");
---
+++ First Steps In Coding - Lab/09. Yard Greening/Program.cs
@@ -0,0 +1,9 @@
+﻿double yard = double.Parse(Console.ReadLine());
+
+double price = yard * 7.61;
+double discount = price * 0.18;
+
+double finalPrice = price - discount;
+
+Console.WriteLine($"{finalPrice} lv.");
+Console.WriteLine($"{discount} lv.");
